9d56491d6cf879bffc764bc77d4a33a21d5982c8
audemar <audemarsadish@gmail.com>
2021-11-18 22:10:25 +0100

    exo5 terminé

---
+++ exo5.js
@@ -0,0 +1,15 @@
+function pink() {
+    look.style.color = "pink"
+}
+function blue() {
+    look.style.color = "blue"
+}
+function purple() {
+    look.style.color = "purple"
+}
+function bold() {
+    look.style.fontWeight = "bold"
+}
+function italic() {
+    look.style.fontstyle = "italic"
+}
